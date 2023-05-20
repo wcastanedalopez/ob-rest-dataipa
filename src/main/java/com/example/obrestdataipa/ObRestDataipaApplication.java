f2a69b4687b2517d350d6d076f7e45bbdb8f5cb5
William <wcastanedalopez@gmail.com>
2023-05-19 22:37:27 -0500

    Base del proyecto

---
+++ src/main/java/com/example/obrestdataipa/ObRestDataipaApplication.java
@@ -0,0 +1,14 @@
+package com.example.obrestdataipa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObRestDataipaApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ObRestDataipaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/obrestdataipa/ObRestDataipaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obrestdataipa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObRestDataipaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
