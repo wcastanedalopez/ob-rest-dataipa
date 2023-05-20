006f0517783bc2d216bfb1b7ff84adc97d8098df
William <wcastanedalopez@gmail.com>
2023-05-20 11:39:35 -0500

    Se han completado los métodos del crud de Books, y se ha cargado el archivo de Postman

--- src/main/java/com/example/obrestdataipa/controller/BookController.java
+++ src/main/java/com/example/obrestdataipa/controller/BookController.java
@@ -2,6 +2,8 @@ package com.example.obrestdataipa.controller;
 
 import com.example.obrestdataipa.entities.Book;
 import com.example.obrestdataipa.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +12,8 @@ import java.util.Optional;
 
 @RestController
 public class BookController {
+
+    private final Logger log = LoggerFactory.getLogger(BookController.class);
 
     BookRepository bookRepository;
 
@@ -39,8 +43,50 @@ public class BookController {
 
     }
 
-    @PostMapping("/api/create")
-    public Book create (@RequestBody Book book) {
-        return bookRepository.save(book);
+    @PostMapping("/api/books")
+    public ResponseEntity<Book> create (@RequestBody Book book) {
+
+        if (book.getId() != null) {
+            log.warn("Traying to create book with id");
+            return ResponseEntity.badRequest().build();
+        }
+        Book aux = bookRepository.save(book);
+        return ResponseEntity.ok(book);
+    }
+
+    @PutMapping("/api/books")
+    public ResponseEntity<Book> update (@RequestBody Book book){
+
+        if (book.getId() == null) {
+            log.warn("Traying to update a non existing book");
+            return ResponseEntity.badRequest().build();
+        }
+        if (!bookRepository.existsById(book.getId()) ) {
+            log.warn("Traying to update a non existing book");
+            return ResponseEntity.notFound().build();
+        }
+        Book aux = bookRepository.save(book);
+        return ResponseEntity.ok(book);
+    }
+
+    @DeleteMapping("/api/books/{id}")
+    public ResponseEntity<Book> deleteBook (@PathVariable Long id) {
+        if (!bookRepository.existsById(id)) {
+            log.warn("Traying to delete a non existing book");
+            return ResponseEntity.notFound().build();
+        }
+        bookRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+    @DeleteMapping("/api/books")
+    public ResponseEntity<Book> deleteAllBook () {
+        if (bookRepository.count() != 0) {
+            log.warn("Traying to delete a non existing book");
+            return ResponseEntity.notFound().build();
+
+        }
+        log.warn("Deleting all books");
+        bookRepository.deleteAll();
+        return ResponseEntity.noContent().build();
     }
 }
